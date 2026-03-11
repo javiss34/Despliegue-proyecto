fdf95302b7b5b83fa97089d086add715c8d012fb
javisr34 <javiersanchezsolera.alu@iespacomolla.es>
2026-03-11 11:50:38 +0100

    IP de AWS configurada en el frontend

--- SCE-FrontEnd-Definitiu/src/services/axiosInstance.js
+++ SCE-FrontEnd-Definitiu/src/services/axiosInstance.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/api',
-  // baseURL: 'http://TU_IP_ELASTICA_DE_AWS:3000/api',
+  baseURL: 'http://100.49.136.165:8080/api',
   withCredentials: true,
 });
 
