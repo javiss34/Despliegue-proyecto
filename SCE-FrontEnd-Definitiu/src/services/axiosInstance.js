52493330f249e305c5164f5d57a5587da6a1d4c4
javisr34 <javiersanchezsolera.alu@iespacomolla.es>
2026-03-11 11:03:14 +0100

    Inicio despliegue proyecto

--- SCE-FrontEnd-Definitiu/src/services/axiosInstance.js
+++ SCE-FrontEnd-Definitiu/src/services/axiosInstance.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: 'http://localhost:3000/api',
+  // baseURL: 'http://TU_IP_ELASTICA_DE_AWS:3000/api',
   withCredentials: true,
 });
 
-// Interceptor para imprimir el endpoint y los datos de la solicitud antes de enviarla
 axiosInstance.interceptors.request.use((config) => {
   console.log('Request endpoint:', `${config.baseURL}${config.url}`);
   console.log('Request data:', config.data);
